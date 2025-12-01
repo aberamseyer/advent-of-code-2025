8b3bec324cfa5859568483687f9b846403264565
Abe Ramseyer <abe.ramseyer@gmail.com>
2025-12-01 08:54:53 -0600

    day 1

---
+++ Day1/Day1.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine(Part1("input.txt"));
+Console.WriteLine(Part2("input.txt"));
+return;
+
+long Part2(string fileName)
+{
+    long dial = 50;
+    long password = 0;
+
+    foreach(var line in File.ReadLines(fileName))
+    {
+        var turn = line[1..].AsSpan().AsLong();
+        password += turn / 100;
+        turn %= 100;
+
+        if (line[0] == 'L')
+        {
+            dial -= turn;
+            if (dial < 0)
+            {
+                if (Math.Abs(dial) != turn)
+                    password++;
+                dial = 100 + dial;
+            }
+        }
+        else
+        {
+            dial += turn;
+            if (dial > 99 && dial % 100 != 0)
+                password++;
+            dial %= 100;
+        }
+
+        if (dial == 0) password++;
+    }
+
+    return password;
+}
+
+long Part1(string fileName)
+{
+    long dial = 50;
+    long password = 0;
+
+    foreach(var line in File.ReadLines(fileName))
+    {
+        var turn = line[1..].AsSpan().AsLong() % 100;
+        if (line[0] == 'L')
+        {
+            dial -= turn;
+            if (dial < 0) dial = 100 + dial;
+        }
+        else dial = (dial + turn) % 100;
+
+        if (dial == 0) password++;
+    }
+
+    return password;
+}
+
+internal static class Extensions
+{
+    internal static long AsLong(this ReadOnlySpan<char> span) => long.Parse(span.ToString());
+}
