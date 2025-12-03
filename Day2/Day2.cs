2f7b2b0864a03411a137a62a7217216f323c1a15
Abe Ramseyer <abe.ramseyer@gmail.com>
2025-12-03 09:48:35 -0600

    day 3

--- Day2/Day2.cs
+++ Day2/Day2.cs
@@ -1,53 +1,53 @@
-﻿using System.Collections.Immutable;
+﻿    using System.Collections.Immutable;
 
-Console.WriteLine(Part1("input.txt"));
-Console.WriteLine(Part2("input.txt"));
+    Console.WriteLine(Part1("input.txt"));
+    Console.WriteLine(Part2("input.txt"));
 
-return;
+    return;
 
-long Part1(string fileName)
-{
-    long total = 0;
-    foreach(var rangeStr in File.ReadAllText(fileName).Split(','))
+    long Part1(string fileName)
     {
-        var range = rangeStr.Split('-').Select(long.Parse).ToImmutableArray();
-        var begin = range[0];
-        var end = range[1];
-
-        for (var i = begin; i <= end; i++)
+        long total = 0;
+        foreach(var rangeStr in File.ReadAllText(fileName).Split(','))
         {
-            var number = i.ToString();
-            if (number[..(number.Length / 2)] == number[(number.Length / 2)..])
-                total += i;
-        }
-    }
+            var range = rangeStr.Split('-').Select(long.Parse).ToImmutableArray();
+            var begin = range[0];
+            var end = range[1];
 
-    return total;
-}
-long Part2(string fileName)
-{
-    long total = 0;
-    foreach(var rangeStr in File.ReadAllText(fileName).Split(','))
-    {
-        var range = rangeStr.Split('-').Select(long.Parse).ToImmutableArray();
-        var begin = range[0];
-        var end = range[1];
-
-        for (var currentNumber = begin; currentNumber <= end; currentNumber++)
-        {
-            var number = currentNumber.ToString();
-            for (var windowSize = number.Length / 2; windowSize >= 1; windowSize--)
+            for (var i = begin; i <= end; i++)
             {
-                var firstChunk = number[..windowSize];
-                if (firstChunk[0] == '0')
-                    break;
-                if (!number.Chunk(windowSize).All(chunk => chunk.SequenceEqual(firstChunk)))
-                    continue;
-                total += currentNumber;
-                break;
+                var number = i.ToString();
+                if (number[..(number.Length / 2)] == number[(number.Length / 2)..])
+                    total += i;
             }
         }
-    }
 
-    return total;
-}
+        return total;
+    }
+    long Part2(string fileName)
+    {
+        long total = 0;
+        foreach(var rangeStr in File.ReadAllText(fileName).Split(','))
+        {
+            var range = rangeStr.Split('-').Select(long.Parse).ToImmutableArray();
+            var begin = range[0];
+            var end = range[1];
+
+            for (var currentNumber = begin; currentNumber <= end; currentNumber++)
+            {
+                var number = currentNumber.ToString();
+                for (var windowSize = number.Length / 2; windowSize >= 1; windowSize--)
+                {
+                    var firstChunk = number[..windowSize];
+                    if (firstChunk[0] == '0')
+                        break;
+                    if (!number.Chunk(windowSize).All(chunk => chunk.SequenceEqual(firstChunk)))
+                        continue;
+                    total += currentNumber;
+                    break;
+                }
+            }
+        }
+
+        return total;
+    }
---
+++ Day3/Day3.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine(Part1("input.txt"));
+Console.WriteLine(Part2("input.txt"));
+
+return;
+
+long Part1(string fileName)
+{
+    long totalJoltage = 0;
+    foreach (var batteryBank in File.ReadLines(fileName))
+    {
+        var batteries = batteryBank.Select(c => c - '0').ToArray();
+        var firstBatteryIndex = 0;
+        for (var i = 0; i < batteries.Length - 1; i++)
+        {
+            if (batteries[i] > batteries[firstBatteryIndex])
+                firstBatteryIndex = i;
+        }
+
+        var secondBatteryIndex = firstBatteryIndex + 1;
+        for (var i = firstBatteryIndex + 1; i < batteries.Length; i++)
+        {
+            if (batteries[i] > batteries[secondBatteryIndex])
+                secondBatteryIndex = i;
+        }
+
+        totalJoltage += batteries[firstBatteryIndex] * 10 + batteries[secondBatteryIndex];
+    }
+
+    return totalJoltage;
+}
+
+long Part2(string fileName)
+{
+    long totalJoltage = 0;
+    foreach (var batteryBank in File.ReadLines(fileName))
+    {
+        var batteries = batteryBank.Select(c => c - '0').ToArray();
+        var reverseDigits = new int[12];
+        long currentJoltage = 0;
+        for (var digit = reverseDigits.Length - 1; digit >= 0; digit--)
+        {
+            var checkFrom = digit == reverseDigits.Length - 1 ? 0 : reverseDigits[digit + 1] + 1;
+            reverseDigits[digit] = checkFrom;
+            for (var cellToCheck = checkFrom; cellToCheck < batteries.Length - digit; cellToCheck++)
+            {
+                if (batteries[cellToCheck] > batteries[reverseDigits[digit]])
+                    reverseDigits[digit] = cellToCheck;
+            }
+
+            var cellJoltage = batteries[reverseDigits[digit]] *
+                              (long)Math.Pow(10, digit);
+            currentJoltage += cellJoltage;
+        }
+
+        totalJoltage += currentJoltage;
+    }
+
+    return totalJoltage;
+}
