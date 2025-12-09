6c9b575672c9d1e4c61955460c53e8a355310e0a
Abe Ramseyer <abe.ramseyer@gmail.com>
2025-12-09 16:01:42 -0600

    2 days
    day 8: don't understand the prompt, couldn't pass part 1
    
    day 9: part 1 is easy, part 2 doesn't work, also I'm sick today

---
+++ Day8/Day8.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+var sw = new Stopwatch();
+sw.Start();
+Console.WriteLine($"{Part1("test.txt")} in {sw.ElapsedMilliseconds}ms");
+sw.Restart();
+Console.WriteLine($"{Part2("test.txt")} in {sw.ElapsedMilliseconds}ms");
+
+return;
+
+long Part1(string fileName)
+{
+    var boxes = File.ReadAllLines(fileName)
+        .Select(l => l.Split(','))
+        .Select(s => new Vector3(long.Parse(s[0]), long.Parse(s[1]), long.Parse(s[2])))
+        .ToList();
+    var distances = boxes
+        .ToDictionary(v => v, v => (from: v, to: Vector3.Zero, distance: float.MaxValue));
+    var circuits = new List<HashSet<Vector3>>();
+    foreach (var box in boxes)
+    {
+        foreach (var otherBox in boxes)
+        {
+            if (otherBox == box) continue;
+            var d = Vector3.Distance(box, otherBox);
+            if (d < distances[box].distance)
+            {
+                distances[box] = (box, otherBox, d);
+            }
+        }
+    }
+
+    var orderedDistances = distances.Values
+        .OrderBy(d => d.distance).ToList();
+    var connectionsMade = 0;
+    foreach (var (from, to, _) in orderedDistances)
+    {
+        if (!distances.ContainsKey(from) && !distances.ContainsKey(to)) continue;
+        var c = circuits.FirstOrDefault(c => c.Contains(from) || c.Contains(to));
+        if (c != null)
+        {
+            c.Add(from);
+            c.Add(to);
+            connectionsMade++;
+        }
+        else
+        {
+            circuits.Add([ from, to ]);
+            connectionsMade += 1;
+        }
+
+        distances.Remove(from);
+        distances.Remove(to);
+        if (connectionsMade == 10)
+            break;
+    }
+
+    return circuits.Select(c => c.Count).OrderDescending()
+        .Take(3).Aggregate(1, (a, b) => a * b);
+}
+
+long Part2(string fileName)
+{
+    var boxes = File.ReadAllLines(fileName)
+        .Select(l => l.Split(','))
+        .Select(s => new Vector3(long.Parse(s[0]), long.Parse(s[1]), long.Parse(s[2])))
+        .ToHashSet();
+
+    return 0;
+}
---
+++ Day9/Day9.cs
@@ -0,0 +1,103 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+var sw = new Stopwatch();
+sw.Start();
+Console.WriteLine($"{Part1("input.txt")} in {sw.ElapsedMilliseconds}ms");
+sw.Restart();
+Console.WriteLine($"{Part2("input.txt")} in {sw.ElapsedMilliseconds}ms");
+
+return;
+
+long Part1(string fileName)
+{
+    var coordinates = File.ReadAllLines(fileName)
+        .Select(l => l.Split(','))
+        .Select(s => new Point(int.Parse(s[0]), int.Parse(s[1])))
+        .ToList();
+    var maxArea = long.MinValue;
+    for (var i = 0; i < coordinates.Count; i++)
+    {
+        for (var j = 0; j < coordinates.Count; j++)
+        {
+            if (i == j) continue;
+            var area = Area(coordinates[i], coordinates[j]);
+            if (area < maxArea) continue;
+            maxArea = area;
+        }
+    }
+    return maxArea;
+}
+
+float Part2(string fileName)
+{
+    var coordinates = File.ReadAllLines(fileName)
+        .Select(l => l.Split(','))
+        .Select(s => new Point(int.Parse(s[0]), int.Parse(s[1])))
+        .ToList();
+    var maxArea = long.MinValue;
+    for (var i = 0; i < coordinates.Count; i++)
+    {
+        for (var j = 0; j < coordinates.Count; j++)
+        {
+            if (i == j) continue;
+            var area = Area(coordinates[i], coordinates[j]);
+            if (area < maxArea) continue;
+
+            var topLeft = new Point(coordinates[i].X, coordinates[j].Y);
+            var bottomRight = new Point(coordinates[j].X, coordinates[i].Y);
+            var segments = new List<List<Point>>
+            {
+                new (){ topLeft, coordinates[j] },
+                new (){ coordinates[j], bottomRight },
+                new (){ bottomRight, coordinates[i] },
+                new (){ coordinates[i], topLeft },
+            };
+            if (segments.All(s =>
+                {
+                    var prev = 0;
+                    for (var k = 1; k < coordinates.Count; k++, prev++)
+                    {
+                        if (ProperIntersection(s[0], s[1], coordinates[prev], coordinates[k]))
+                             return false;
+                    }
+
+                    return true;
+                }))
+            {
+                maxArea = area;
+            }
+        }
+    }
+    return maxArea;
+}
+
+long Area(Point a, Point b) => Math.BigMul(
+    Math.Abs(a.X - b.X)+1, Math.Abs(a.Y - b.Y)+1
+);
+
+bool ProperIntersection(Point p1, Point q1,
+    Point p2, Point q2)
+{
+    // Vector cross product (p,q,r) = (q-p) × (r-p)
+    double Cross(Point a, Point b, Point c) =>
+        (b.X - a.X) * (c.Y - a.Y) -
+        (b.Y - a.Y) * (c.X - a.X);
+
+    // Orientation of triplet (a,b,c):
+    //  >0 : counter‑clockwise
+    //  <0 : clockwise
+    //   0 : collinear
+    var o1 = Cross(p1, q1, p2);
+    var o2 = Cross(p1, q1, q2);
+    var o3 = Cross(p2, q2, p1);
+    var o4 = Cross(p2, q2, q1);
+
+    // Proper intersection iff orientations differ
+    var proper = (o1 > 0 && o2 < 0 || o1 < 0 && o2 > 0) &&
+                 (o3 > 0 && o4 < 0 || o3 < 0 && o4 > 0);
+
+    return proper;
+}
+
+record Point(int X, int Y);
