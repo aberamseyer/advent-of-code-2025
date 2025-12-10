91702ae6783cd5fe4ec0396c55ce6634d65e5945
Abe Ramseyer <abe.ramseyer@gmail.com>
2025-12-10 08:39:57 -0600

    c# needs permutation support

---
+++ Day10/Day10.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+var sw = new Stopwatch();
+sw.Start();
+Console.WriteLine($"{Part1("test.txt")} in {sw.ElapsedMilliseconds}ms");
+sw.Restart();
+Console.WriteLine($"{Part2("test.txt")} in {sw.ElapsedMilliseconds}ms");
+
+return;
+
+long Part1(string fileName)
+{
+    return File.ReadAllLines(fileName)
+        .Select(l => Regex.Match(l, @"\[([\.\#]+)\] (\([\d,]+\) ?)+ \{([\d,]+)\}"))
+        .Select(m => new Machine(
+            m.Groups[1].Value.Trim().Select(c => c == '#').ToArray(),
+            m.Groups[2].Captures.Select(c => c.Value.Trim('(',')',' ').Split(',').Select(int.Parse).ToList()).ToList(),
+            m.Groups[3].Value.Split(',').Select(int.Parse).ToList()
+        )).Sum(machine =>
+        {
+            var presses = new int[machine.Buttons.Count];
+            while (!CheckMachine(machine, ref presses))
+            {
+                // this doesn't correctly do permutations, so it doesn't work
+                for (var i = 0; i < presses.Length; i++)
+                {
+                    presses[i] += 1;
+                    if (CheckMachine(machine, ref presses)) break;
+                    presses[i] -= 1;
+                }
+
+                for (var i = 0; i < presses.Length; i++)
+                {
+                    presses[i]++;
+                }
+            }
+            return presses.Sum();
+        });
+}
+
+long Part2(string fileName)
+{
+    return 0;
+}
+
+bool CheckMachine(Machine m, ref int[] presses)
+{
+    var lightState = new bool[m.Lights.Length];
+    for (var pressIndex = 0; pressIndex < presses.Length; pressIndex++)
+    {
+        foreach (var _ in Enumerable.Range(0, presses[pressIndex]))
+        {
+            foreach (var b in m.Buttons[pressIndex])
+            {
+                lightState[b] = !lightState[b];
+            }
+        }
+    }
+
+    for (var i = 0; i < m.Lights.Length; i++)
+    {
+        if (m.Lights[i] != lightState[i])
+            return false;
+    }
+
+    return true;
+}
+record Machine(bool[] Lights, List<List<int>> Buttons, List<int> Joltages);
--- Day9/Day9.cs
+++ Day9/Day9.cs
@@ -1,11 +1,10 @@
 ﻿using System.Diagnostics;
-using System.Numerics;
 
 var sw = new Stopwatch();
 sw.Start();
 Console.WriteLine($"{Part1("input.txt")} in {sw.ElapsedMilliseconds}ms");
 sw.Restart();
-Console.WriteLine($"{Part2("input.txt")} in {sw.ElapsedMilliseconds}ms");
+Console.WriteLine($"{Part2("test.txt")} in {sw.ElapsedMilliseconds}ms");
 
 return;
 
@@ -46,19 +45,13 @@ float Part2(string fileName)
 
             var topLeft = new Point(coordinates[i].X, coordinates[j].Y);
             var bottomRight = new Point(coordinates[j].X, coordinates[i].Y);
-            var segments = new List<List<Point>>
-            {
-                new (){ topLeft, coordinates[j] },
-                new (){ coordinates[j], bottomRight },
-                new (){ bottomRight, coordinates[i] },
-                new (){ coordinates[i], topLeft },
-            };
-            if (segments.All(s =>
+            var vertices = new List<Point>
+                { topLeft, coordinates[j], bottomRight, coordinates[i] };
+            if (vertices.All(v =>
                 {
-                    var prev = 0;
-                    for (var k = 1; k < coordinates.Count; k++, prev++)
+                    for (int prev = 0, k = 1; k < coordinates.Count; prev=k++)
                     {
-                        if (ProperIntersection(s[0], s[1], coordinates[prev], coordinates[k]))
+                        if (Left(coordinates[prev], coordinates[k], v))
                              return false;
                     }
 
@@ -76,28 +69,19 @@ long Area(Point a, Point b) => Math.BigMul(
     Math.Abs(a.X - b.X)+1, Math.Abs(a.Y - b.Y)+1
 );
 
-bool ProperIntersection(Point p1, Point q1,
-    Point p2, Point q2)
+int AreaSign(Point a, Point b, Point c)
 {
-    // Vector cross product (p,q,r) = (q-p) × (r-p)
-    double Cross(Point a, Point b, Point c) =>
-        (b.X - a.X) * (c.Y - a.Y) -
-        (b.Y - a.Y) * (c.X - a.X);
+    var area2 = (b.X - a.X) * (double)(c.Y - a.Y) -
+                (c.X - a.X) * (double)(b.Y - a.Y);
 
-    // Orientation of triplet (a,b,c):
-    //  >0 : counter‑clockwise
-    //  <0 : clockwise
-    //   0 : collinear
-    var o1 = Cross(p1, q1, p2);
-    var o2 = Cross(p1, q1, q2);
-    var o3 = Cross(p2, q2, p1);
-    var o4 = Cross(p2, q2, q1);
+    if (area2 > 0.5) return 1;
+    if (area2 < -0.5) return -1;
+    return 0;
+}
 
-    // Proper intersection iff orientations differ
-    var proper = (o1 > 0 && o2 < 0 || o1 < 0 && o2 > 0) &&
-                 (o3 > 0 && o4 < 0 || o3 < 0 && o4 > 0);
-
-    return proper;
+bool Left(Point a, Point b, Point c)
+{
+    return AreaSign(a, b, c) < 0;
 }
 
 record Point(int X, int Y);
